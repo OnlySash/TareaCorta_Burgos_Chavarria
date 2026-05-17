c3570f06201f13b0a0f2beb622bcef54e1548079
OnlySash <schavar03@gmail.com>
2026-05-17 15:33:59 -0600

    Fase 2 y anàlisis completa

--- README.md
+++ README.md
@@ -1,17 +1,55 @@
 ## Race Condition: 
-Identifique exactamente cuáles líneas del código de Fase 1
-constituyen una race condition. ¿Por qué la instrucción en_cruce = 1 sola no es
-suficiente para garantizar exclusión mutua?
+#Identifique exactamente cuáles líneas del código de Fase 1 constituyen una race condition. ¿Por qué la instrucción en_cruce = 1 sola no es suficiente para garantizar exclusión mutua?
+R/ Las siguientes partes constituyen race condition ya que son variables compartidas entre hilos y se modifican simultaneamente. La instruccion 
+```c 
+    en_cruce = 1
+```
+no garantiza exclusion mutua porque no es una operacion atomica ni esta protegida
+
+```c
+    if (en_cruce == 1) {
+      accidentes++;
+      printf("ACCIDENTE detectado con %s\n", nombre);
+    }
+    
+    int time = 2500 + rand_r(&semilla) % 3000;
+    en_cruce = 1; 
+    usleep(time);
+    en_cruce = 0;
+
+    cola[numero_de_carril]--;
+    vehiculos_cruzados++;
+    cruzados[numero_de_carril]++;
+```
+
 ## Invariante del semáforo: 
-¿Qué invariante garantiza el semáforo binario en Fase 2?
-Explíquelo con sus propias palabras. ¿Por qué se inicializa en 1 y no en 0?
+#¿Qué invariante garantiza el semáforo binario en Fase 2? Explíquelo con sus propias palabras. ¿Por qué se inicializa en 1 y no en 0?
+
+R/ El uso de semaforos permite que un carro pueda cruzar correctamente. Mientras un hilo tenga el semáforo, los demás deben esperar.
+
+```c
+sem_wait(&semaforo_cruce);
+sem_post(&semaforo_cruce);
+```
+
+Se inicializa en `1` porque el cruce está inicialmente libre y puede ser usado por el carro. Si se inicializara en `0`, todos los hilos quedarían bloqueados porque ningún hilo podría pasar por el semáforo para entrar al cruce.
+
 ## Overhead: 
-¿Por qué la Fase 2 suele tomar más tiempo que la Fase 1? ¿Este
-overhead es aceptable? ¿En qué tipo de sistema NO sería aceptable?
-## Experimento: Presente los resultados del experimento de escala (N_VEHICULOS =
-5, 20, 50). ¿Cómo cambia la tasa de accidentes al aumentar el número de vehículos?
-¿Tiene sentido ese comportamiento?
+#¿Por qué la Fase 2 suele tomar más tiempo que la Fase 1? ¿Este overhead es aceptable? ¿En qué tipo de sistema NO sería aceptable?
+R/ Esto se debe a tiempos de espera dados por el semaforo y entre hilos, es aceptable ya que no es tanto tiempo en comparacion. No serìa aceptable en sistemas de tiempo real estricto.
+
+## Experimento: Presente los resultados del experimento de escala (N_VEHICULOS = 5, 20, 50). 
+#¿Cómo cambia la tasa de accidentes al aumentar el número de vehículos? ¿Tiene sentido ese comportamiento?
+R/ Bajo la fase 1 la tasa de accidentes crece de manera proporcional a la cantidad de carros, en teorìa. Pero la practica muestra que con 50 carros por carril caen los accidentes, esto tiene sentido pensando en la cantidad de carriles. Con la fase 2 independientemente de los carros, no hay choques.
+5= 3
+20= 17
+50= 4
+
 ## Extensión (opcional, bonus +0.5): 
-¿Cómo modificaría el sistema para permitir que
-más de un vehículo cruce simultáneamente (carril de doble vía)? ¿Qué tipo de
-semáforo usaría?
+#¿Cómo modificaría el sistema para permitir que más de un vehículo cruce simultáneamente (carril de doble vía)? ¿Qué tipo de semáforo usaría?
+
+R/Para ello hay que aumentar los valores en el sem, lo que harìa una "doble via" y debe usarse "semaforo_cruce"
+
+```c
+sem_init(&semaforo_cruce, 0, 2);
+```
--- interseccion.c
+++ interseccion.c
@@ -1,56 +1,9 @@
-/* FASE 2 ESPERADA DEL CRITICA - REFERENCIA
-sem_wait(&semaforo_cruce); // P() - intentar entrar al cruce
-en_cruce = 1;
-// ... cruzar ...
-en_cruce = 0;
-sem_post(&semaforo_cruce); // V() - liberar el cruce
-*/
-
-/*========================================================
-SIMULADOR DE INTERSECCION DE TRAFICO - CI-0117
-Carriles: 4 | Vehiculos por carril: 10
-========================================================*/
-/*--- FASE 1: Sin sincronizacion ---*/
-/*[Norte-001] entrando al cruce
-[Sur-001] entrando al cruce <- ACCIDENTE con Norte-001
-[Norte-001] cruce completado
-...
---- FASE 2: Con semaforos ---
-[Norte-001] sem_wait() -> cruzando
-[Norte-001] sem_post() -> cruce libre
-[Sur-001] sem_wait() -> cruzando
-...
-======== REPORTE FINAL ========
-FASE 1 (sin sincronizacion):
-Total vehiculos: 40
-Accidentes: 7 <- debe ser > 0
-Vehiculos/carril: Norte=10 Sur=10 Este=10 Oeste=10
-Tiempo simulacion: 0.312 segundos
-FASE 2 (con semaforos):
-Total vehiculos: 40
-Accidentes: 0 <- debe ser exactamente 0
-Vehiculos/carril: Norte=10 Sur=10 Este=10 Oeste=10
-Tiempo simulacion: 0.418 segundos
-ANALISIS:
-Overhead de sincronizacion: +0.106 seg (+34.0%)
-=======================================================*/
-
-/*
- * interseccion.c
- * Simulador de Interseccion de Trafico Concurrente
- * CI-0117 Programacion Paralela y Concurrente - I Ciclo 2026
- *
- * FASE 1: Sin sincronizacion (demuestra race condition)
- * FASE 2: Con semaforos POSIX (lo hace tu compañero)
- *
- * Compilar Fase 1: gcc -Wall -Wextra -std=c11 interseccion.c -o interseccion
- * -lpthread Compilar Fase 2: gcc -Wall -Wextra -std=c11 -DFASE2 interseccion.c
- * -o interseccion -lpthread
+/*Simulador de Interseccion de Trafico Concurrente
+  CI-0117 Programacion Paralela y Concurrente - I Ciclo 2026
+ 
+  Compilar: gcc -Wall -Wextra -std=c11 interseccion.c -o interseccion -lpthread 
  */
 
-/* Necesario para usleep(), rand_r() y clock_gettime() en C11 */
-
-// 
 #define _XOPEN_SOURCE 600 // El #define le dice al compilador antes de leer cualquier header: "activame las extensiones POSIX"
 // y ahí ya reconoce algunas fucniones usadas en el código como usleep() y rand_r() que no son parte del estándar C11 pero si de POSIX
 // esto porque el profe pide el std 11 porque creo que si se usa un std mas nuevo el compilador no se queja con los warnings
@@ -63,13 +16,18 @@ Overhead de sincronizacion: +0.106 seg (+34.0%)
 #include <unistd.h>
 
 #define N_CARRILES 4
-#define N_VEHICULOS 10
+#define N_VEHICULOS 50
 
 const char *nombres[N_CARRILES] = {"Norte", "Sur", "Este", "Oeste"};
 
 int vehiculos_cruzados = 0;
+int vehiculos_cruzados_sem = 0;
 int accidentes = 0;
+int accidentes_sem = 0;
 int en_cruce = 0;
+int en_cruce_sem = 0;
+double tiempo = 0;
+double tiempo_sem = 0;
 int cola[N_CARRILES];
 int cruzados[N_CARRILES];
 
@@ -95,7 +53,7 @@ void *hilo_carril(void *dato_del_carril) {
     sprintf(nombre, "%s-%d", nombres[numero_de_carril], i);
 
     cola[numero_de_carril]++;
-    printf("[%s] cruzando\n", nombre);
+    //printf("[%s] cruzando\n", nombre);
 
     // aqui es donde se da la Race condition en Fase 1, y donde se debe usar semaforos en Fase 2
     if (en_cruce == 1) {
@@ -103,12 +61,12 @@ void *hilo_carril(void *dato_del_carril) {
       printf("ACCIDENTE detectado con %s\n", nombre);
     }
     
-    int tiempo = 2500 + rand_r(&semilla) % 3000;
+    int time = 2500 + rand_r(&semilla) % 3000;
     en_cruce = 1; //marcar que hay un vehiculo en el cruce (sin proteccion)
-    usleep(tiempo);
+    usleep(time);
     en_cruce = 0;
 
-    printf("[%s] cruzo\n", nombre);
+    //printf("[%s] cruzo\n", nombre);
 
     // aqui se actualizan los contadores sin proteccion (tambien es race condition)
     cola[numero_de_carril]--;
@@ -118,7 +76,7 @@ void *hilo_carril(void *dato_del_carril) {
   return NULL;
 }
 
-int main(void) {
+void no_sincronizacion(){
   pthread_t hilos[N_CARRILES];
   int ids[N_CARRILES];
 
@@ -127,13 +85,8 @@ int main(void) {
     cruzados[i] = 0;
     ids[i] = i;
   }
-
-  sem_init(&semaforo_cruce, 0, 1);
-  sem_init(&mutex_contadores, 0, 1);
-
-  printf("tarea de interseccion de vehiculos\n");
-  printf("Vehiculos por carril: %d\n\n", N_VEHICULOS);
-
+  
+  printf("--- FASE 1: Sin sincronizacion ---\n");
   double inicio = tiempo_actual();
 
   for (int i = 0; i < N_CARRILES; i++) {
@@ -144,16 +97,103 @@ int main(void) {
   }
 
   double fin = tiempo_actual();
+  tiempo = fin - inicio;
 
-  printf("\nDatos de la simulacion:\n");
-  printf("Vehiculos cruzados: %d\n", vehiculos_cruzados);
+  printf("======== REPORTE ======== \n FASE 1 (sin sincronizacion):\n");
+  printf("Total vehiculos: %d\n", vehiculos_cruzados);
   printf("Accidentes: %d\n", accidentes);
-  printf("Norte: %d | Sur: %d | Este: %d | Oeste: %d\n", cruzados[0],
+  printf("Vehiculos/carril Norte: %d | Sur: %d | Este: %d | Oeste: %d\n", cruzados[0],
          cruzados[1], cruzados[2], cruzados[3]);
-  printf("Tiempo total: %.3f segundos\n", fin - inicio);
+  printf("Tiempo simulacion: %.3f segundos\n", tiempo);
+}
 
+void *hilo_carril_sem(void *dato_del_carril) {
+  int numero_de_carril = *(int *)dato_del_carril;
+  unsigned int semilla = numero_de_carril + 1;
+
+  for (int i = 1; i <= N_VEHICULOS; i++) {
+    char nombre[20];
+    sprintf(nombre, "%s-%d", nombres[numero_de_carril], i);
+
+    cola[numero_de_carril]++;
+
+    // aqui es donde se da la Race condition en Fase 1
+    sem_wait(&semaforo_cruce);
+    printf("[%s] cruzando\n", nombre);
+
+    //La variable en_cruce_sem ya no tiene forma de ser 1
+    if (en_cruce_sem == 1) {
+      accidentes_sem++;
+      printf("ACCIDENTE detectado con %s\n", nombre);
+    }
+
+    int time = 2500 + rand_r(&semilla) % 3000;
+    en_cruce_sem = 1; //marcar que hay un vehiculo en el cruce (sin proteccion)
+    usleep(time);
+    en_cruce_sem = 0;
+    printf("[%s] cruzo\n", nombre);
+    sem_post(&semaforo_cruce);
+
+    sem_wait(&mutex_contadores);
+    cola[numero_de_carril]--;
+    vehiculos_cruzados_sem++;
+    cruzados[numero_de_carril]++;
+    sem_post(&mutex_contadores);
+  }
+
+  return NULL;
+}
+
+void semaforo(){
+  sem_init(&semaforo_cruce, 0, 1);
+  sem_init(&mutex_contadores, 0, 1);
+
+  pthread_t t_sem [N_CARRILES];
+  int ids_s [N_CARRILES];
+
+  for (int i = 0; i < N_CARRILES; i++) {
+    cola[i] = 0;
+    cruzados[i] = 0;
+    ids_s [i] = i;
+  }
+  
+  printf("\n--- FASE 2: Con semaforos ---\n");
+  double inicio = tiempo_actual();
+
+  for (int i = 0; i < N_CARRILES; i++) {
+    pthread_create(&t_sem[i], NULL, hilo_carril_sem, &ids_s[i]);
+  }
+  for (int i = 0; i < N_CARRILES; i++) {
+    pthread_join(t_sem[i], NULL);
+  }
+
+  double fin = tiempo_actual();
+  tiempo_sem = fin - inicio;
+  printf("======== REPORTE ======== \n FASE 2 (con semaforos):\n");
+  printf("Total vehiculos: %d\n", vehiculos_cruzados_sem);
+  printf("Accidentes: %d\n", accidentes_sem);
+  printf("Vehiculos/carril Norte: %d | Sur: %d | Este: %d | Oeste: %d\n", cruzados[0],
+         cruzados[1], cruzados[2], cruzados[3]);
+  printf("Tiempo simulacion: %.3f segundos\n", tiempo_sem);
+  
   sem_destroy(&semaforo_cruce);
   sem_destroy(&mutex_contadores);
+}
+
+int main(void) {
+  printf("========================================================\n");
+  printf("SIMULADOR DE INTERSECCION DE TRAFICO - CI-0117\n");
+  printf("========================================================\n");
+  printf("Carriles: %d | Vehiculos por carril: %d\n",N_CARRILES, N_VEHICULOS);
+  printf("========================================================\n");
+  
+  no_sincronizacion();
+  semaforo();
+
+  double overhead = tiempo_sem - tiempo;
+  double porcent = (overhead / tiempo) * 100;
+  printf("ANALISIS: \n Overhead de sincronizacion: %.3f  seg (%.2f%%)\n", overhead, porcent);
+  printf("========================================================\n");
 
   return 0;
 }
